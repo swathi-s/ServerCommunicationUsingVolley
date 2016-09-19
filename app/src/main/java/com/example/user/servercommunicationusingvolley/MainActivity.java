814c51d565b642e0adf7179fce760a6ff5b291ee
swathi-s <swathi.ehr@gmail.com>
2016-09-19 17:18:56 +0530

    Client server communication is done using volley library

--- app/src/main/java/com/example/user/servercommunicationusingvolley/MainActivity.java
+++ app/src/main/java/com/example/user/servercommunicationusingvolley/MainActivity.java
@@ -1,13 +1,86 @@
 package com.example.user.servercommunicationusingvolley;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button getDataBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getDataBtn = (Button) findViewById(R.id.button);
+        getDataBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Get Data button has clicked by the user",Toast.LENGTH_SHORT).show();
+                getDataFromServer();
+            }
+        });
+
+    }
+
+    public void getDataFromServer()
+    {
+        String tag_json_obj = "json_obj_req";
+        String url = "http://192.168.1.7/androidService.php";
+
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Loading ...");
+        progressDialog.show();
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST,
+                url,null,
+                new Response.Listener<JSONObject>(){
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d("REsponse",response.toString());
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        progressDialog.hide();
+                    }
+                },new Response.ErrorListener(){
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d("Response","Error : "+error.getMessage());
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                progressDialog.hide();
+            }
+        }){
+            @Override
+            protected Map<String,String> getParams() {
+                Map<String,String> params = new HashMap<String, String>();
+                params.put("type","tw");
+                return params;
+            }
+        };
+
+        AppController.getInstance().addToRequestQueue(jsonObjectRequest,tag_json_obj);
     }
 }
